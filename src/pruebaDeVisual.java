dded6f7ddc09da9859cfe7bee226c8839e1e95ba
yeiyei09 <yeffergomez02@gmail.com>
2024-05-24 20:24:00 -0500

    Yefferson nuevo print

--- src/pruebaDeVisual.java
+++ src/pruebaDeVisual.java
@@ -1,5 +1,8 @@
 public class pruebaDeVisual {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        PruebaDeClase prueba = new PruebaDeClase();
+        System.out.println(".()");
+        System.out.println("Este es parte del nuevo archivo");
     }
 }
